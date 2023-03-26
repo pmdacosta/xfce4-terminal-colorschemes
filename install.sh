1755a8c7e3f70a3cc32ca1b3a657e3ee3de82e6c
Pedro Costa <125618804+pmdacosta@users.noreply.github.com>
2023-03-26 14:43:27 +0100

    variable a refactored for a more meaningful name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-a="$(dirname $0)"
-cd "$a"
+script_dir="$(dirname $0)"
+cd "$script_dir"
 dir="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/terminal/colorschemes"
 echo "Copying .theme files to $dir ..."
 mkdir -p "$dir"
